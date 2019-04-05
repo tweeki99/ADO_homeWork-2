a3082f72ee56e3e82b1050ed4c52679eb6ac4c0f
tweeki99 <danil.shipilov.99@gmail.com>
2019-04-05 20:55:04 +0600

    hw2

---
+++ hwAdo-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using hwAdo2.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwAdo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+
+            dataService.CreateGruppaTable();
+        }
+    }
+}
---
+++ hwAdo2.DataAccess/DataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hwAdo2.DataAccess
+{
+    public class DataService
+    {
+        private readonly string _connectionString;
+
+        private readonly DbProviderFactory _providerFactory;
+
+        public DataService()
+        {
+            _providerFactory = DbProviderFactories.GetFactory(ConfigurationManager.ConnectionStrings["HomeConnectionString"].ProviderName);
+            _connectionString = ConfigurationManager.ConnectionStrings["HomeConnectionString"].ConnectionString;
+        }
+
+        public void CreateGruppaTable()
+        {
+            using (var connection = _providerFactory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                connection.ConnectionString = _connectionString;
+                connection.Open();
+                command.CommandText = @"CREATE TABLE [dbo].[Table] ( [Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), [Name] NVARCHAR(50) NOT NULL)";
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
